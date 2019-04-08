a6e352b68399243f5cb7de77d01c0732cdfeb69e
Nayan Vaish <nayanvaish@Nayans-MBP.fios-router.home>
2019-04-07 22:28:49 -0400

    changes to move to postgresql and separate container for database

--- musicSharing/storeLinks/views.py
+++ musicSharing/storeLinks/views.py
@@ -17,9 +17,9 @@ import sys
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
