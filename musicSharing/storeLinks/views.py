480c8b8bca79493b31d61a9a0e43d968136e32c6
Nayan Vaish <nayanvaish@Nayans-MBP.fios-router.home>
2019-04-07 22:32:14 -0400

    changes to move logging from debug to info

--- musicSharing/storeLinks/views.py
+++ musicSharing/storeLinks/views.py
@@ -17,9 +17,9 @@ import sys
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
